649e0f6009485030fd36a9806ca8cf7ff1a8ec5d
aecio <aecio.jorge@gmail.com>
2020-02-04 18:20:57 -0400

    add bonus point

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : Eatable
+{
+    [SerializeField]
+    private BoxCollider2D collider;
+
+    [SerializeField]
+    private TMPro.TextMeshProUGUI text;
+
+    void Start()
+    {
+        collider.enabled = false;
+
+        if(GameState.level == 0)
+        {
+            points = 100;
+        }
+        else if(GameState.level == 1)
+        {
+            points = 500;
+        }
+        else
+        {
+            points = 1000;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+            HandleObjectWasEaten();
+        }
+    }
+
+    public void SetPoints()
+    {
+        if(GameState.level == 0)
+        {
+            points = 100;
+        }
+        else if(GameState.level == 1)
+        {
+            points = 500;
+        }
+        else
+        {
+            points = 1000;
+        }
+    }
+
+    public void ResetFruit()
+    {
+        text.gameObject.SetActive(false);
+        collider.enabled = false;
+        SetPoints();
+    }
+}
---
+++ Assets/Scripts/SpawnFruit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFruit : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fruit;
+
+    [SerializeField]
+    private BoxCollider2D collider;
+
+    [SerializeField]
+    private SpriteRenderer fruitSpriteRenderer;
+
+    [SerializeField]
+    private List<Sprite> fruitSprites;
+
+    void Start()
+    {
+        StartTimerToSpawnFruit();
+    }
+
+    IEnumerator StartFruitTimer()
+    {
+        int timer = 0;
+
+        while(timer < 30)
+        {
+            timer++;
+            yield return new WaitForSeconds(1);
+        }
+
+        fruitSpriteRenderer.enabled = true;
+        collider.enabled = true;
+    }
+
+    public void StartTimerToSpawnFruit()
+    {
+        fruitSpriteRenderer.sprite = fruitSprites[GameState.level];
+        fruitSpriteRenderer.enabled = false;
+        StartCoroutine(StartFruitTimer());
+    }
+}
